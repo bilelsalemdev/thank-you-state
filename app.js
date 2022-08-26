3f2e5b77b35ba71c7857cc799df8a374b7db4280
BILEL SALEM <70206023+bilelsalem2020@users.noreply.github.com>
2022-08-26 13:08:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const submit=document.querySelector('.submit');
 const container = document.querySelector('.container');
 const rating = document.querySelector('.rating');
 const span = document.querySelector('span')
-numbers.forEach((number) => {
+numbers.forEach((number,index) => {
     number.addEventListener('click' , (e)=> {
-        number.style.backgroundColor='hsl(25, 97%, 53%)';
-        number.style.color='white';
+        number.classList.add('selected')
+        
         submit.style.color='hsl(25, 97%, 53%)';
         submit.style.backgroundColor='white';
         span.innerHTML=' ' + e.target.innerText + ' ';
-        
+        cancelSelection(index);
     })
 })
+function cancelSelection(i){
+    numbers.forEach((number,index) => {
+        if(index !== i) {
+            number.classList.remove('selected')
+        }
+    })}
 submit.addEventListener('click',()=>{
         container.style.display='none';
         rating.style.display='flex';
